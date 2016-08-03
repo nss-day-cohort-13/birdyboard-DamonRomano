e202e16fb389a885dcaa95066af99d93f21e97c9
Damon Romano <rdromano@mac.com>
2016-08-03 14:42:01 -0500

    refactored Main Menu to built it in not the hardest-way-possible.

--- birdyboard.py
+++ birdyboard.py
@@ -1,3 +1,30 @@
-import stuff from places
-from magic import spells
+# import stuff from places
+# from magic import spells
+
+print ('1. Create New User Account')
+print ('2. Select User')
+print ('3. View All Chirps')
+print ('4. Make New Public Chirp')
+print ('5. Make New Private Chirp')
+print ('6. Exit Birdyboard')
+
+
+selection = input("Please Make A Selection:")
+if selection == '1':
+    print ("Create New User Account")
+elif selection == '2':
+    print ("Select User")
+elif selection == '3':
+    print ("View All Chirps")
+elif selection == '4':
+    print ("Make New Public Chirp")
+elif selection == '5':
+    print ("Make New Private Chirp")
+elif selection == '6':
+    print ("Exit Birdyboard")
+else:
+    print ("The thing you want doesn't exist.  You want the impossible.")
+
+
+
 
